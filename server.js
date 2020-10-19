41dd0fe43545af7e31e5c747bcebaa2569b19157
Garlic Fiend <suzannaakins@gmail.com>
2020-10-19 14:05:32 -0700

    candidate id route created

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //import express
 const express = require('express');
+const { ppid } = require('process');
 //add the port designation
 const PORT = process.env.PORT || 3001;
 //add the app expression
@@ -18,6 +19,60 @@ const db = new sqlite3.Database('./db/election.db', err => {
     }
     console.log('Connected to the election database.');
 });
+
+// get ALL potential candidates listed
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// //get a SINGLE candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
+// // DELETE a candidate
+// db.run(`DELETE FROM candidates WHERE id=?`, 1, function (err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+// // CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
 
 //default response for any other request(Not Found) catch all
 app.use((req, res) => {
