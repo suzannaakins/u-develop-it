8584e13785c5d28ebcbe2f07e65f28ca659292fe
Garlic Fiend <suzannaakins@gmail.com>
2020-10-20 15:12:08 -0700

    update candidate routes to join w/ party data

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // get ALL potential candidates listed
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if (err) {
@@ -40,8 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 //get a SINGLE candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
