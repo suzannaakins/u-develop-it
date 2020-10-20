7df0842d7d41b2d3165a046089053d4e93a59bb8
Garlic Fiend <suzannaakins@gmail.com>
2020-10-20 15:29:48 -0700

    party routes added to server.js

--- server.js
+++ server.js
@@ -106,6 +106,53 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({ 
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+
+        res.json({ 
+            message: 'successfully deleted', changes: this.changes 
+        });
+    });
+});
+
 //default response for any other request(Not Found) catch all
 app.use((req, res) => {
     res.status(404).end();
