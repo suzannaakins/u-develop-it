d8dc52d355660e14e86d879be6b1825568c29382
Garlic Fiend <suzannaakins@gmail.com>
2020-10-21 14:16:27 -0700

    DELETE voter route

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -89,4 +89,17 @@ router.put('/voter/:id', (req, res) => {
     });
 });
 
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+
+    db.run(sql, req.params.id, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({ message: 'deleted', changes: this.changes });
+    });
+});
+
 module.exports = router;
