00ebe36d6fc7ce88f968e9c44d1322d873a720af
Garlic Fiend <suzannaakins@gmail.com>
2020-10-21 13:53:55 -0700

    party routes modularized

--- server.js
+++ server.js
@@ -17,56 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-//view parties and their descriptions
-app.get('/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-//view a party based on id
-app.get('/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-//delete a party from the candidates table, by party id
-app.delete('/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function (err, result) {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            return;
-        }
-
-        res.json({
-            message: 'successfully deleted', changes: this.changes
-        });
-    });
-});
-
 //default response for any other request(Not Found) catch all
 app.use((req, res) => {
     res.status(404).end();
